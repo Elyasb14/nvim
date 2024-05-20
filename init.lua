9839c5534ed0c109f6a482dbbd3f43c1a9c654cb
Elyas Bianchi <elyasb99@gmail.com>
2024-05-21 00:27:09 +0100

    jj remap to go into normal mode

--- init.lua
+++ init.lua
@@ -86,6 +86,9 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- use jj in insert mode to change back to normal mode
+vim.keymap.set('i', 'jj', '<Esc>')
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
