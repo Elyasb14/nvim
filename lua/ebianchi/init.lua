613a9458ec6efb122fc0aa03a31c97c1e14e0673
Elyas Bianchi <elyasb99@gmail.com>
2024-06-01 22:38:49 -0700

    ripping off primeagens nvim layout

--- lua/ebianchi/init.lua
+++ lua/ebianchi/init.lua
@@ -1,34 +1,3 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.g.have_nerd_font = true
-vim.o.signcolumn = 'no'
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.hlsearch = true
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-require 'ebianchi.lazy_init'
 require 'ebianchi.remap'
+require 'ebianchi.set'
+require 'ebianchi.lazy_init'
