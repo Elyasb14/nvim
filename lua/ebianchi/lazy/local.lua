d7adf1f7bb23c9ddd710e4fc36eea09fcbe8cf9a
Elyas Bianchi <elyasb99@gmail.com>
2024-06-02 20:07:01 -0700

    local lua packages

---
+++ lua/ebianchi/lazy/local.lua
@@ -0,0 +1,12 @@
+local local_plugins = {
+  {
+    "touch.nvim",
+    dir = "~/code/touch-nvim/",
+    config = function()
+      local touch = require("touch-nvim")
+      touch:setup()
+    end
+  }
+}
+
+return local_plugins
