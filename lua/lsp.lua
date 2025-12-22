470004ae350a4f382c2766e3af8ac69b86af5493
Elyas Bianchi <elyasb99@gmail.com>
2025-12-22 11:58:40 -0800

    back

---
+++ lua/lsp.lua
@@ -0,0 +1,46 @@
+vim.lsp.config['lua_ls'] = {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            }
+        }
+    }
+}
+vim.lsp.enable('lua_ls')
+
+vim.lsp.config["zls"] = {
+    cmd = { 'zls' },
+    filetypes = { 'zig', 'zir' },
+}
+vim.lsp.enable('zls')
+
+local cmp = require 'cmp'
+cmp.setup({
+    completion = { completeopt = 'menu,menuone,noinsert' },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'path' },
+    },
+    mapping = {
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LspFormatting", {}),
+    callback = function(event)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = event.buf,
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+            end,
+        })
+    end,
+})
