ea6f7f0a5c9aa15da537edf2aae7bfaccae78802
Elyas Bianchi <elyasb99@gmail.com>
2025-12-22 11:29:57 -0800

    work

---
+++ lua/init.lua
@@ -0,0 +1,51 @@
+vim.pack.add {
+    'git@github.com:nvim-treesitter/nvim-treesitter',
+    'git@github.com:neovim/nvim-lspconfig',
+    'git@github.com:hrsh7th/nvim-cmp.git',
+    'git@github.com:hrsh7th/cmp-nvim-lsp',
+    'git@github.com:hrsh7th/cmp-path',
+    'git@github.com:folke/tokyonight.nvim',
+    'git@github.com:rose-pine/neovim'
+}
+
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+local ts = require 'nvim-treesitter'
+ts.setup({
+    install_dir = vim.fn.stdpath('data') .. '/site',
+    ensure_installed = { "zig", "c", "bash", "lua" },
+    highlight = { enable = true },
+    auto_install = true,
+    build = ':TSUpdate',
+})
+
+-- start treesitter on any file
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { '<filetype>' },
+    callback = function() vim.treesitter.start() end,
+})
+
+-- highlight what you copied
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+require("lua.lsp")
+require("lua.color")
