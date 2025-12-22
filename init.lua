f1e22a8b41fd2e90504abc17928ce9614f09020a
Elyas Bianchi <elyasb99@gmail.com>
2025-12-21 17:17:42 -0800

    autocomplete

--- init.lua
+++ init.lua
@@ -1,52 +1,66 @@
 vim.pack.add {
-    	'git@github.com:nvim-treesitter/nvim-treesitter',
+    'git@github.com:nvim-treesitter/nvim-treesitter',
 	'git@github.com:neovim/nvim-lspconfig',
+    'git@github.com:hrsh7th/nvim-cmp.git',
+    'git@github.com:hrsh7th/cmp-nvim-lsp',
+    'git@github.com:hrsh7th/cmp-path'
 }
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
-vim.treesitter.build = ':TSUpdate'
-vim.treesitter.ensure_installed = {'zig', 'c', 'bash', 'lua'}
-vim.treesitter.highlight = true
-
-vim.lsp.config['lua-language-server'] = {
-       -- Command and arguments to start the server.
-       cmd = { 'lua-language-server' },
-       -- Filetypes to automatically attach to.
-       filetypes = { 'lua' },
-       -- Sets the "workspace" to the directory where any of these files is found.
-       -- Files that share a root directory will reuse the LSP server connection.
-       -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-       root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-       -- Specific settings to send to the server. The schema is server-defined.
-       -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-       settings = {
-         Lua = {
-           runtime = {
-             version = 'LuaJIT',
-           }
-         }
-       }
-     }
-
-
-
-
-     vim.cmd[[set completeopt+=menuone,noselect,popup]]
-vim.lsp.start({
-  name = 'lua_ls',
-  cmd = {'L'},
-  on_attach = function(client, bufnr)
-    vim.lsp.completion.enable(true, client.id, bufnr, {
-      autotrigger = true,
-      convert = function(item)
-        return { abbr = item.label:gsub('%b()', '') }
-      end,
-    })
-  end,
+local ts = require 'nvim-treesitter'
+ts.setup({
+  install_dir = vim.fn.stdpath('data') .. '/site',
+  ensure_installed = { "zig", "c", "bash", "lua" },
+  highlight = { enable = true },
+  build = ':TSUpdate',
+  indent = true, 
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
+
+vim.lsp.config['lua_ls'] = {
+  -- Command and arguments to start the server.
+  cmd = { 'lua-language-server' },
+  -- Filetypes to automatically attach to.
+  filetypes = { 'lua' },
+  -- Sets the "workspace" to the directory where any of these files is found.
+  -- Files that share a root directory will reuse the LSP server connection.
+  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  -- Specific settings to send to the server. The schema is server-defined.
+  -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  }
+}
 vim.lsp.enable('lua_ls')
+
+local cmp = require 'cmp'
+cmp.setup({
+    completion = { completeopt = 'menu,menuone,noinsert' },
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'path' },
+    },
+    mapping = {
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    },
+})
